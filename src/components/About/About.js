0f593a7b928f1b02108f759204c2a12d0ab83c9d
NicoPicotto <nicopicotto@hotmail.com>
2022-02-23 22:33:25 -0300

    Adding responsive

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,22 +1,22 @@
 import { React } from 'react';
-import { useSpring, animated, config } from 'react-spring';
+// import { useSpring, animated, config } from 'react-spring';
 import './About.css';
 
 const About = () => {
-	const props = useSpring({
-		from: { opacity: 0, y: -100 },
-		to: { opacity: 1, y: 0 },
-		config: config.molasses,
-	});
+	// const props = useSpring({
+	// 	from: { opacity: 0, y: -100 },
+	// 	to: { opacity: 1, y: 0 },
+	// 	config: config.molasses,
+	// });
 
 	return (
-		<animated.div className='aboutContainer' style={props} id='about'>
+		<div className='aboutContainer' id='about'>
 			<div className='aboutText'>
-				<h3 className='aboutHeader'>Hello you! I'm</h3>
+				<h3 className='aboutHeader'>Hey you! I'm...</h3>
 				<h1 className='aboutName'>Nico Picotto</h1>
 				<h3 className='aboutFooter'>Graphic Designer and Frontend Developer</h3>
 			</div>
-		</animated.div>
+		</div>
 	);
 };
 
